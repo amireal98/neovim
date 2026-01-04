38d2792c3d9393e9266e9ce80761e5d37c8fc507
amireal98 <amireal98@proton.me>
2026-01-04 16:17:27 -0600

    fixed indent

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,21 +1,22 @@
 return {
-	{
-		'nvim-treesitter/nvim-treesitter',
-		lazy = false,
-		build = ":TSUpdate",
-		opts = {
-			indent = { enable = true },
-			highlight = { enable = true },
-			folds = { enable = true },
-			ensure_installed = {
-				"lua",
-				"bash",
-				"diff",
-				"gitignore",
-				"markdown",
-				"c",
-				"nix",
-			},
-		},
-	},
+  {
+    'nvim-treesitter/nvim-treesitter',
+    lazy = false,
+    build = ":TSUpdate",
+    opts = {
+      indent = { enable = true },
+      highlight = { enable = true },
+      folds = { enable = false },
+      ensure_installed = {
+        "lua",
+        "bash",
+        "diff",
+        "gitignore",
+        "markdown",
+        "c",
+        "nix",
+        "hyprlang",
+      },
+    },
+  },
 }
