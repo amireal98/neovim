26e5c917180c3b7db95f167ad5593b5219f50821
amireal98 <amireal98@proton.me>
2026-01-06 16:24:24 -0600

    added bufferline

---
+++ lua/ui/bufferline.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    opts = {
+      options = {
+        mode = "buffers",
+        close_command = "bdelete! %d",
+        right_mouse_command = "bdelete! %d",
+        left_mouse_command = "buffer %d",
+        middle_mouse_command = nil,
+        indicator = {
+          icon = '▎',
+        },
+        buffer_close_icon = '󰅖',
+        modified_icon = '● ',
+        close_icon = '󰅖',
+        left_trunc_marker = ' ',
+        right_trunc_marker = ' ',
+      },
+    },
+  },
+}
